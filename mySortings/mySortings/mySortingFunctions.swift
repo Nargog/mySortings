0945c699700a87c7588c1683fef2f1b6a376d49a
Mats Hammarqvist <mats@hammarqvist.eu>
2020-10-24 21:24:31 +0200

    implementation ny variant insertion sort

--- mySortings/mySortings/mySortingFunctions.swift
+++ mySortings/mySortings/mySortingFunctions.swift
@@ -7,6 +7,89 @@
 
 import Foundation
 
+/* insertion sort: function insSort
+ 
+ i ← 1
+ while i < length(A)
+     j ← i
+     while j > 0 and A[j-1] > A[j]
+         swap A[j] and A[j-1]
+         j ← j - 1
+     end while
+     i ← i + 1
+ end while
+ 
+ 
+ 
+ */
+
+
+func insSort (A_in:[Int])-> [Int]{
+    var i = 1
+    var j = 0
+    var A = A_in
+    while i < A.count {
+        j = i
+        while j > 0 && A[j-1] > A[j] {
+            A.swapAt(j, j-1)
+            j -= 1
+            
+        }
+        i += 1
+    }
+    return A
+}
+
+// Min lite fulare variant med flytt från osorterad till sorterad array
+
+func insertionSort(inArray:[Int])->[Int]{
+    //skapa en variabel med osorterade värden och en tom array med sorterade värden
+    var unsorted=inArray
+    var sorted:[Int] = []
+    //lägg till första värdet osorterad lista som start i sorterad lista
+    sorted.append(unsorted.remove(at: 0))
+
+    // lite hjälpvariabler
+    var foundPlace : Bool
+    var i = 0
+    var valueToSort = 0
+
+    while unsorted.count > 0  { //töm första objektet unsorted från början
+        foundPlace = false
+        valueToSort = unsorted.remove(at: 0)
+        i = 0
+        
+        print(unsorted.count, i)
+        while i < sorted.count && !foundPlace { //om vi hittar större värde insert före detta värde och om vi hittat rätt plats avsluta sökande efter plats
+            
+            if sorted[i] > valueToSort{
+                sorted.insert(valueToSort, at: i)
+                foundPlace = true
+            } else {
+            
+            i += 1
+            
+            }
+            
+        }
+        //om det inte finns något högre värde så är värdet högst/sist
+        if !foundPlace {
+            sorted.append(valueToSort)
+        }
+        //loopa sorted
+        //insert första unserted på första plats med högre värde efter i sorted. Ta bort värde i unsorted
+        // sluta loop då vi inte behöver gå vidare
+ 
+    }
+    
+
+   sorted.append(contentsOf: unsorted) //det blir en kvar i array unsorted :(
+    
+    
+    return sorted
+}
+
+
 // Selection sort. Letar hela tiden efter öägsta värde https://en.wikipedia.org/wiki/Selection_sort
 func selectionSort(inArray:[Int])->[Int]{
     var unsorted=inArray
